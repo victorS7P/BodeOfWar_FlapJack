58c94c693b8ce9a9b5b80c65d53bfd754334ee3f
Gabriel Barroso <biel.barroso@hotmail.com>
2022-04-13 11:22:18 -0300

    Adicionadas mudanças visuais

--- FlapJack/Forms/Main.Designer.cs
+++ FlapJack/Forms/Main.Designer.cs
@@ -31,7 +31,6 @@
             this.btnPlay = new System.Windows.Forms.Button();
             this.btnExit = new System.Windows.Forms.Button();
             this.lblVersion = new System.Windows.Forms.Label();
-            this.lblTitle = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnPlay
@@ -39,12 +38,15 @@
             this.btnPlay.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.btnPlay.AutoSize = true;
+            this.btnPlay.BackColor = System.Drawing.Color.Red;
+            this.btnPlay.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnPlay.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.btnPlay.Location = new System.Drawing.Point(354, 202);
             this.btnPlay.Name = "btnPlay";
             this.btnPlay.Size = new System.Drawing.Size(112, 31);
             this.btnPlay.TabIndex = 1;
             this.btnPlay.Text = "Jogar";
-            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.UseVisualStyleBackColor = false;
             this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
             // 
             // btnExit
@@ -52,12 +54,14 @@
             this.btnExit.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.btnExit.AutoSize = true;
+            this.btnExit.BackColor = System.Drawing.Color.Red;
+            this.btnExit.ForeColor = System.Drawing.Color.White;
             this.btnExit.Location = new System.Drawing.Point(354, 239);
             this.btnExit.Name = "btnExit";
             this.btnExit.Size = new System.Drawing.Size(112, 36);
             this.btnExit.TabIndex = 2;
             this.btnExit.Text = "Sair";
-            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.UseVisualStyleBackColor = false;
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
             // lblVersion
@@ -66,7 +70,7 @@
             this.lblVersion.AutoSize = true;
             this.lblVersion.BackColor = System.Drawing.Color.Transparent;
             this.lblVersion.Font = new System.Drawing.Font("Consolas", 11.25F);
-            this.lblVersion.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lblVersion.ForeColor = System.Drawing.Color.Red;
             this.lblVersion.Location = new System.Drawing.Point(676, 405);
             this.lblVersion.Name = "lblVersion";
             this.lblVersion.Size = new System.Drawing.Size(112, 36);
@@ -74,31 +78,19 @@
             this.lblVersion.Text = "Server v@.@\r\nFlapjack v1.0";
             this.lblVersion.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
-            // lblTitle
-            // 
-            this.lblTitle.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.lblTitle.AutoSize = true;
-            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 20.25F);
-            this.lblTitle.ForeColor = System.Drawing.Color.White;
-            this.lblTitle.Location = new System.Drawing.Point(324, 162);
-            this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(174, 37);
-            this.lblTitle.TabIndex = 3;
-            this.lblTitle.Text = "Bodes of War";
-            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImage = global::FlapJack.Properties.Resources.BoW_Bg;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.lblTitle);
             this.Controls.Add(this.lblVersion);
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.btnPlay);
+            this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -119,7 +111,6 @@
         private System.Windows.Forms.Button btnPlay;
         private System.Windows.Forms.Button btnExit;
         private System.Windows.Forms.Label lblVersion;
-        private System.Windows.Forms.Label lblTitle;
     }
 }
 
