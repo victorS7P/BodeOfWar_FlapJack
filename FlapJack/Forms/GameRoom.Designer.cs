87e698c252b41736f20b9fe057032dfadb3aecc8
Gabriel Barroso <biel.barroso@hotmail.com>
2022-04-25 09:21:24 -0300

    arrumado bg e botões

--- FlapJack/Forms/GameRoom.Designer.cs
+++ FlapJack/Forms/GameRoom.Designer.cs
@@ -48,7 +48,13 @@
             this.gbxCards.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+<<<<<<< Updated upstream
             this.gbxCards.Location = new System.Drawing.Point(15, 291);
+=======
+            this.gbxCards.BackColor = System.Drawing.Color.DarkRed;
+            this.gbxCards.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.gbxCards.Location = new System.Drawing.Point(15, 437);
+>>>>>>> Stashed changes
             this.gbxCards.Name = "gbxCards";
             this.gbxCards.Size = new System.Drawing.Size(898, 340);
             this.gbxCards.TabIndex = 11;
@@ -68,21 +74,30 @@
             // 
             // plsRoom
             // 
+            this.plsRoom.BackColor = System.Drawing.Color.DarkRed;
             this.plsRoom.Location = new System.Drawing.Point(12, 25);
             this.plsRoom.Name = "plsRoom";
             this.plsRoom.Players = null;
             this.plsRoom.Size = new System.Drawing.Size(172, 260);
             this.plsRoom.TabIndex = 7;
+            this.plsRoom.Load += new System.EventHandler(this.plsRoom_Load);
             // 
             // GameRoom
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+<<<<<<< Updated upstream
             this.ClientSize = new System.Drawing.Size(925, 643);
+=======
+            this.BackgroundImage = global::FlapJack.Properties.Resources.bg_menus;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(800, 643);
+>>>>>>> Stashed changes
             this.Controls.Add(this.pnlCards);
             this.Controls.Add(this.gbxCards);
             this.Controls.Add(this.plsRoom);
             this.Controls.Add(this.lblPlayers);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "GameRoom";
             this.Text = "Match";
             this.Load += new System.EventHandler(this.GameMatch_Load);
